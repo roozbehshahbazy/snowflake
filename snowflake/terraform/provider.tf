7facb1a2461880ca3ed6e807b3824ce6029993e9
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-12 10:15:50 +1200

    revert the changes

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -2,8 +2,9 @@
 provider "snowflake" {
   organization_name = "AOJAKXH"
   account_name      = "YN03913"
+  user              = "TERRAFORM_GITHUB"
 
   authenticator              = "WORKLOAD_IDENTITY"
-  workload_identity_provider = "AZURE"
+  workload_identity_provider = "OIDC"
 }
 
