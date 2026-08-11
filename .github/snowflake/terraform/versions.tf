06703ed327466056a468a5a491cdc0240a0c26b6
roozbehshahbazy <roozbehshahbazy@yahoo.com>
2026-08-11 21:02:55 +1200

    Create versions.tf

---
+++ .github/snowflake/terraform/versions.tf
@@ -0,0 +1,9 @@
+terraform {  
+  backend "azurerm" {
+    subscription_id      = "dadbc2ea-904e-4b51-9974-8581e34e539d"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformforsnowflake"
+    container_name       = "snowflake"
+    key                  = "snowflake.terraform.tfstate"
+  }
+}
