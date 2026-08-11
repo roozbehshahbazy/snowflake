8eb8d8759a349b7b804f53ae5f48f76d959df402
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-11 23:49:37 +1200

    terraform fmt

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -3,7 +3,7 @@ provider "snowflake" {
   organization_name = "AOJAKXH"
   account_name      = "YN03913"
   role              = "TERRAFORM_ROLE"
-  user = "TERRAFORM_GITHUB"
+  user              = "TERRAFORM_GITHUB"
 
   authenticator              = "WORKLOAD_IDENTITY"
   workload_identity_provider = "OIDC"
