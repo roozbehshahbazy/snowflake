f122d6f6a004688ccefeb8d8631a55faa0858486
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-12 00:15:08 +1200

    removing user

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -3,7 +3,6 @@ provider "snowflake" {
   organization_name = "AOJAKXH"
   account_name      = "YN03913"
   role              = "TERRAFORM_ROLE"
-  user              = "TERRAFORM_GITHUB"
 
   authenticator              = "WORKLOAD_IDENTITY"
   workload_identity_provider = "OIDC"
