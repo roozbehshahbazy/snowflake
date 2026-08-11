0a2fa5d2af5b74b47192cba752e6ecc9fb59716b
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-11 23:38:29 +1200

    terraform fmt

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -1,8 +1,8 @@
 
 provider "snowflake" {
   organization_name = "AOJAKXH"
-  account_name = "YN03913"
-  role    = "TERRAFORM_ROLE"
+  account_name      = "YN03913"
+  role              = "TERRAFORM_ROLE"
 
   authenticator              = "WORKLOAD_IDENTITY"
   workload_identity_provider = "OIDC"
