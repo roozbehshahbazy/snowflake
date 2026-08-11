e68ef240831109c08500b1e1f0bc1480ae9c0576
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-12 00:49:45 +1200

    update provider

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -2,8 +2,8 @@
 provider "snowflake" {
   organization_name = "AOJAKXH"
   account_name      = "YN03913"
-  role              = "TERRAFORM_ROLE"
+  user              = "TERRAFORM_GITHUB"
 
   authenticator              = "WORKLOAD_IDENTITY"
-  workload_identity_provider = "azure"
+  workload_identity_provider = "OIDC"
 }
