e815f66efd0094ea381678dcc801c680df02cb24
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-12 14:24:33 +1200

    adding role

--- snowflake/terraform/provider.tf
+++ snowflake/terraform/provider.tf
@@ -3,6 +3,7 @@ provider "snowflake" {
   organization_name = "AOJAKXH"
   account_name      = "YN03913"
   user              = "GITHUB_ACTIONS_SNOWFLAKE"
+  role              = "GITHUB_ACTIONS_DEPLOYER"
 
   authenticator              = "WORKLOAD_IDENTITY"
   workload_identity_provider = "OIDC"
