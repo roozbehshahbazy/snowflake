e250395c3b9268ed05bedd073eab2955625a92eb
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-13 22:12:07 +1200

    adding warehouse

--- snowflake/my_dcm_project/sources/definitions/bronze.sql
+++ snowflake/my_dcm_project/sources/definitions/bronze.sql
@@ -1,1 +1,9 @@
+define warehouse DCM_DEMO_1_WH
+with 
+    warehouse_size = "X-SMALL"
+    auto_suspend = 300
+    comment = 'For Quickstart Demo of DCM Projects PrPr'
+;
+
+
 define SCHEMA CICD.BRONZE;
