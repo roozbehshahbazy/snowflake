b4b12bd54cc90377c21274b7bad7009c5d3ef625
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-13 23:55:13 +1200

    testing

--- snowflake/my_dcm_project/sources/definitions/bronze.sql
+++ snowflake/my_dcm_project/sources/definitions/bronze.sql
@@ -1,11 +1,11 @@
--- define warehouse DCM_DEMO_1_WH
--- with 
---     warehouse_size = "X-SMALL"
---     auto_suspend = 300
---     comment = 'For Quickstart Demo of DCM Projects PrPr'
--- ;
+define warehouse DCM_DEMO_1_WH
+with 
+    warehouse_size = "X-SMALL"
+    auto_suspend = 300
+    comment = 'For Quickstart Demo of DCM Projects PrPr'
+;
 
--- grant USAGE on warehouse DCM_DEMO_1_WH to role GITHUB_ACTIONS_DEPLOYER;
+grant USAGE on warehouse DCM_DEMO_1_WH to role GITHUB_ACTIONS_DEPLOYER;
 
 
 define SCHEMA CICD.BRONZE;
