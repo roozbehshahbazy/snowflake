c8880baf7dcc9df25554e87f8c38c34a08b1fc13
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 10:50:20 +1200

    TEST SOMETHING ELSE

--- snowflake/terraform/role_assignment.tf
+++ snowflake/terraform/role_assignment.tf
@@ -1,5 +1,5 @@
 
 resource "snowflake_grant_account_role" "dcm_developer_to_roozbeh" {
-  role_name        = snowflake_account_role.dcm_developer.name
+  role_name        = "DCM_DEVELOPER"
   parent_role_name = "ROOZBEH1984"
 }
