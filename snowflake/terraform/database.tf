3cf8e9f0f454fd9c64102671315e79cd25431168
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 09:53:03 +1200

    create a DCM deployment role

--- snowflake/terraform/database.tf
+++ snowflake/terraform/database.tf
@@ -1,3 +1,7 @@
 resource "snowflake_database" "cicd" {
   name = "CICD"
 }
+
+resource "snowflake_database" "platform" {
+  name = "platform"
+}
---
+++ snowflake/terraform/role.tf
@@ -0,0 +1,4 @@
+resource "snowflake_account_role" "dcm_developer" {
+  name    = "DCM_DEVELOPER"
+  comment = "Role for DCM developers"
+}
