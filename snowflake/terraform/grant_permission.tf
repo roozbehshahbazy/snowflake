35559489877d80535d598401c01902d3bba6cbdd
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 20:37:38 +1200

    fix the permission

--- snowflake/terraform/grant_permission.tf
+++ snowflake/terraform/grant_permission.tf
@@ -60,6 +60,6 @@ resource "snowflake_grant_privileges_to_account_role" "dcm_developer_to_bronze_s
   account_role_name = snowflake_account_role.dcm_developer.name
 
   on_schema {
-    schema_name = snowflake_schema.retail_bronze.name
+    schema_name = snowflake_schema.retail_bronze.fully_qualified_name 
   }
 }
