6bf9e77342b8883d84fb5faa6bedf4d4bcae2a46
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 14:22:14 +1200

    create a new user for dmc

--- snowflake/terraform/role_assignment.tf
+++ snowflake/terraform/role_assignment.tf
@@ -3,3 +3,10 @@ resource "snowflake_grant_account_role" "dcm_developer_to_roozbeh" {
   role_name = snowflake_account_role.dcm_developer.name
   user_name = "ROOZBEH1984"
 }
+
+
+
+resource "snowflake_grant_account_role" "dcm_developer_to_git_user" {
+  role_name = snowflake_account_role.dcm_developer.name
+  user_name = snowflake_service_user.github_actions_dmc_snowflake.display_name
+}
---
+++ snowflake/terraform/user.tf
@@ -0,0 +1,10 @@
+resource "snowflake_service_user" "github_actions_dmc_snowflake" {
+  name = "GITHUB_ACTIONS_SNOWFLAKE_DMC"
+
+  default_workload_identity {
+  oidc {
+    issuer  = "https://token.actions.githubusercontent.com"
+    subject = "repo:roozbehshahbazy/snowflake:environment:dev"
+  }
+  }
+}
