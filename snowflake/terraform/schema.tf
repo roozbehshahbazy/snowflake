a6115f26c4f1c1405b0753b49f98663b88330ec8
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 18:08:29 +1200

    bronze schema

--- snowflake/terraform/schema.tf
+++ snowflake/terraform/schema.tf
@@ -1,4 +1,9 @@
-resource "snowflake_schema" "project" {
+resource "snowflake_schema" "platform_project" {
   database = "PLATFORM"
   name     = "PROJECT"
+}
+
+resource "snowflake_schema" "retail_bronze" {
+  database = "RETAIL"
+  name     = "BRONZE"
 }
