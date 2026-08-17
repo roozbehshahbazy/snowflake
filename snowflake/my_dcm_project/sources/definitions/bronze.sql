0e90639d4c3631a5e62e2633c2db6e7cfdf1c86e
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 12:39:29 +1200

    CHANGING MANIFEST FILE

--- snowflake/my_dcm_project/sources/definitions/bronze.sql
+++ snowflake/my_dcm_project/sources/definitions/bronze.sql
@@ -1,13 +1,2 @@
-define warehouse DCM_DEMO_1_WH
-with 
-    warehouse_size = "X-SMALL"
-    auto_suspend = 300
-    comment = 'For Quickstart Demo of DCM Projects PrPr'
-;
+define SCHEMA RETAIL.BRONZE;
 
-grant USAGE on warehouse DCM_DEMO_1_WH to role GITHUB_ACTIONS_DEPLOYER;
-
-
-define SCHEMA CICD.BRONZE;
-
-define SCHEMA CICD.SILVER;
