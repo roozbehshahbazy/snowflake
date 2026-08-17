3e6f67c7c1776d3026ee4eb5983052b02fa08002
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-17 14:25:01 +1200

    terraform fmt

--- snowflake/terraform/user.tf
+++ snowflake/terraform/user.tf
@@ -2,9 +2,9 @@ resource "snowflake_service_user" "github_actions_dmc_snowflake" {
   name = "GITHUB_ACTIONS_SNOWFLAKE_DMC"
 
   default_workload_identity {
-  oidc {
-    issuer  = "https://token.actions.githubusercontent.com"
-    subject = "repo:roozbehshahbazy/snowflake:environment:dev"
-  }
+    oidc {
+      issuer  = "https://token.actions.githubusercontent.com"
+      subject = "repo:roozbehshahbazy/snowflake:environment:dev"
+    }
   }
 }
