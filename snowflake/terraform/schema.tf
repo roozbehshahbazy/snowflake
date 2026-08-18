15bc469e631248961a0f574e8a93508e470bfc42
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-18 16:13:07 +1200

    two additional schema creation

--- snowflake/terraform/schema.tf
+++ snowflake/terraform/schema.tf
@@ -3,7 +3,14 @@ resource "snowflake_schema" "project" {
   name     = "PROJECT"
 }
 
-resource "snowflake_schema" "retail_bronze" {
-  database = "RETAIL"
-  name     = "BRONZE"
+
+resource "snowflake_schema" "platform_security" {
+  database = "PLATFORM"
+  name     = "SECURITY"
+}
+
+
+resource "snowflake_schema" "platform_git" {
+  database = "PLATFORM"
+  name     = "GIT"
 }
