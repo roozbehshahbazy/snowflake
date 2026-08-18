bb8ebf77123be9f419ac944fddb44ba7cfd7ed05
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-18 17:00:12 +1200

    adding more permission

--- snowflake/terraform/grant_permission.tf
+++ snowflake/terraform/grant_permission.tf
@@ -20,3 +20,15 @@ resource "snowflake_grant_privileges_to_account_role" "platform_usage" {
 }
 
 
+# Grant CREATE SECRET on the schema
+resource "snowflake_grant_privileges_to_account_role" "dcm_developer_create_secret" {
+  account_role_name = "DCM_DEVELOPER"
+  privileges        = ["CREATE SECRET"]
+
+  on_schema {
+    schema_name = "PLATFORM.SECURITY"
+  }
+}
+
+
+
