0f9b63afc3eda28095e759465da477683264e033
Roozbeh Shahbazirad <roozbeh.shahbazirad@z.co.nz>
2026-08-22 13:07:30 +1200

    define a new schema

--- snowflake/terraform/schema.tf
+++ snowflake/terraform/schema.tf
@@ -1,4 +1,10 @@
 resource "snowflake_schema" "project" {
-  database = "PLATFORM"
+  database = snowflake_database.platform.name
   name     = "PROJECT"
+}
+
+
+resource "snowflake_schema" "refinery_bronze_schema" {
+  name     = "bronze"
+  database = snowflake_database.refinery.name
 }
